7ba165adaa0e4200305ee140dc0366f1fa4f4e10
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-05 21:22:36 +0300

    lesson2

---
+++ zadacha1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Дано число. Проверить кратно ли оно 7 и 23
+
+Console.WriteLine("enter number:");
+int num = int.Parse(Console.ReadLine());
+// and &&
+if ((num %7==0) && (num %23==0))
+    Console.WriteLine("cratno 3 and 7");
+else
+      Console.WriteLine("ne kratno 3 and 7");
+//Console.WriteLine (n %7==0 && n%23==0 ? "cratno" : "nekratno"); тернарный оператор
+
+//if (coll % 7 == 0)
+//{
+//if (coll % 23 == 0)
+//{
+//posicia = "Chislo kratno";
+//}
+//if (coll % 23 == 0)
+//{
+//posicia = "Chislo kratno";
+//}
+//else { posicia = "Chislo ne kratno"; }
+//}
+//else { posicia = "Chislo ne kratno"; };
---
+++ zadacha2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Определить номер четверти плоскости,
+// в которой находится точка с координатами Х и У, 
+//причем X ≠ 0 и Y ≠ 0
+
+Console.WriteLine("enter x:");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("enter y:");
+int y = int.Parse(Console.ReadLine());
+ 
+if ((x>0) && (y>0))
+{
+Console.WriteLine("1");
+}
+if ((x<0) && (y>0))
+{
+Console.WriteLine("2");
+}
+if ((x<0) && (y<0))
+{
+Console.WriteLine("3");
+}
+if ((x>0) && (y<0))
+{
+Console.WriteLine("4");
+}
+if ((x==0) && (y==0))
+{
+    Console.WriteLine(" wrong x no 0 and y no 0");
+}
